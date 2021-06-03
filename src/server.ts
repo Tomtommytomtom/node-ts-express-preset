2fd0a007bd19ac1c44099ad1a82e7172587a3959
Tom Pillokat <tom.pillokat@googlemail.com>
2021-06-03 17:19:32 +0200

    Updated dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from "express"
 import cors from "cors"
-import * as bodyParser  from "body-parser"
 
 
 const initHttpServer = ( httpPort: number ) => {
   const app = express();
   app.use(cors());
-  app.use(bodyParser.json());
 
   app.get('/',(_,res) => {
       res.send("Hello World!")
